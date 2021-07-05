52f34eb3a2d7b9613f2d32d33b4fa63693c1fdcb
esteradimplere <ester@adimplere.com.br>
2021-07-05 20:37:45 -0300

    FEAT: Store and Get into database

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 from fastapi import FastAPI 
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':{'name':"Ester"}}
+@app.get('/blog')
+def index(limit=10,published:bool=True, sort: Optional[str] = None):
+    if published:
+        return {'data':f'{limit} published blogs from the db'}
+    else:
+        return {'data':f'{limit} blogs from the db'}
 
-@app.get('/about')
-def about():
-    return {'data':{'about page'}}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublishec blogs'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data':id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data': {'1','2'}}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data':f"Blog is created with {request.title}"}
+
