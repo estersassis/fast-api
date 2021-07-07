03fe9d0ea3f7786f10809b7c64bc6acd829d4bf6
esteradimplere <ester@adimplere.com.br>
2021-07-07 14:30:24 -0300

    FEAT: Added relationships

--- blog/main.py
+++ blog/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Depends, status, Response, HTTPException
-from . import schemas, models
+from . import schemas, models, hashing
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from typing import List
@@ -14,22 +14,22 @@ def get_db():
     finally:
         db.close()
 
-@app.post('/blog', status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body)
+@app.post('/blog', status_code=status.HTTP_201_CREATED, tags=['blogs'])
+def create_blogs(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body,user_id=1)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
 
-@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id, db: Session = Depends(get_db)):
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT, tags=['blogs'])
+def destroy_blogs(id, db: Session = Depends(get_db)):
     db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
     db.commit()
     return Response('done')
 
-@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
-def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED, tags=['blogs'])
+def update_blogs(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog {id} is not available")   
@@ -37,22 +37,37 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     db.commit()
     return "updated"
 
-@app.get('/blog', response_model=List[schemas.ShowBlog])
-def all(db: Session = Depends(get_db)):
+@app.get('/blog', response_model=List[schemas.ShowBlog], tags=['blogs'])
+def get_all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}',status_code=200, response_model=schemas.ShowBlog)
-def show(id, response: Response, db: Session = Depends(get_db)):
+@app.get('/blog/{id}',status_code=200, response_model=schemas.ShowBlog, tags=['blogs'])
+def get_blog_per_id(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog {id} is not available")
     return blog
 
-@app.post('/user')
+@app.post('/user', response_model=schemas.ShowUser, tags=['users'])
 def create_user(request:schemas.User, db: Session = Depends(get_db)):
-    new_user = models.User(name=request.name, email=request.email, password=request.password)
+    new_user = models.User(name=request.name, email=request.email, password=hashing.Hash.bcrypting(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@app.get('/user', response_model=List[schemas.ShowUser], tags=['users'])
+def get_all_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    return users
+
+@app.get('/user/{id}', status_code=200, response_model=schemas.ShowUser, tags=['users'])
+def get_user_per_id(id, response: Response, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User {id} doesn't exist")
+    return user
+
+if __name__ == "__main__":
+    uvicorn.run(app,host="127.0.0.1", port=9000)
